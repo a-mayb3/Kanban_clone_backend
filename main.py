e3be0498704e2eef04d6b5f0cfc0f2960c352a2f
Borgia Leiva <edbole@campusaula.com>
2026-01-27 12:46:45 +0100

    Added some joke and info root endpoint and some TODOs

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI, HTTPException, Depends
 
-from routers.tasks import router as tasks_router
 from routers.projects import router as projects_router
+from routers.users import router as users_router
 
 app = FastAPI()
 
-app.include_router(tasks_router)
+app.include_router(users_router)
 app.include_router(projects_router)
 
 """ping pong :)"""
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+"""Gives project url"""
+@app.get("/sources")
+def source():
+    return {"url": "https://github.com/a-mayb3/Kanban_clone_backend"}
+
+## TODO: Add root endpoint that gives basic info about the API
+## TODO: Add more detailed error handling and logging
+## TODO: Implement authentication and authorization mechanisms

839728cf6604d2c27f560552b7729b2f072e5c71
Borgia Leiva <edbole@campusaula.com>
2026-02-10 11:37:30 +0100

    Made ProjectUser return full project info

--- schemas/projects_users.py
+++ schemas/projects_users.py
@@ -1,10 +1,11 @@
 from typing import List
 
 from pydantic import ConfigDict
-from schemas.projects import ProjectBase
+from schemas.projects import ProjectFull
 from schemas.users import UserBase
 
 class ProjectUserBase(UserBase):
     model_config = ConfigDict(from_attributes=True)
     
-    projects: List[ProjectBase]
+    projects: List[ProjectFull]
+    
